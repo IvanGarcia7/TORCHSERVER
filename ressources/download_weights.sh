173b38ee58a197e800fe06d3536e6eac5ea279e4
Ivan <ivanbolero@hotmail.com>
2022-06-03 00:48:42 +0200

    Fix download_weights

--- ressources/download_weights.sh
+++ ressources/download_weights.sh
@@ -2,7 +2,7 @@
 # Download latest models from https://github.com/ultralytics/yolov5/releases
 
 python - <<EOF
-from utils.google_utils import attempt_download
+from utils.downloads import attempt_download
 
 for x in ['s', 'm', 'l', 'x']:
     attempt_download(f'yolov5{x}.pt')
